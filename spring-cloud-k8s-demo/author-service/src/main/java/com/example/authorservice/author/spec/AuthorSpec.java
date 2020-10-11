c0d92bd73527c79717912c3d5d031f5fc4064cb8
Hai Hua (Ryan) Tan <ryanhhtan@gmail.com>
2020-10-11 01:44:39 -0700

    fixed invalid param

--- spring-cloud-k8s-demo/author-service/src/main/java/com/example/authorservice/author/spec/AuthorSpec.java
+++ spring-cloud-k8s-demo/author-service/src/main/java/com/example/authorservice/author/spec/AuthorSpec.java
@@ -20,21 +20,36 @@ public class AuthorSpec {
 		return (root, query, builder) -> root.get("id").in(parseIdList(ids));
 	};
 
+	public static Specification<Author> nameLike(final String name) {
+		return (root, query, builder) -> builder.like(root.get("name"), makeSqlLikeString(name));
+	}
+
 	private static List<Long> parseIdList(final String ids) {
 		return Arrays.asList(ids.split(",")).stream().map(Long::new).collect(Collectors.toList());
+	}
+
+	private static String makeSqlLikeString(final String input) {
+		return String.format("%s%s%s", "%", input, "%");
 	}
 
 	public static Specification<Author> query(Map<String, String> queryParams) {
 		if (Objects.isNull(specs)) {
 			initSpecs();
 		}
-		return queryParams.keySet().stream()
-				.map(param -> specs.get(param).apply(queryParams.get(param)))
+		return queryParams.keySet().stream().map(
+				param -> specs.getOrDefault(param, AuthorSpec::unspecified).apply(queryParams.get(param)))
 				.reduce(null, (acc, cur) -> Objects.isNull(acc) ? cur : acc.and(cur));
+	}
+
+
+	public static Specification<Author> unspecified(final String param) {
+		return null;
 	}
 
 	public static void initSpecs() {
 		specs = new HashMap<>();
 		specs.put("id.in", AuthorSpec::idIn);
+		specs.put("name.like", AuthorSpec::nameLike);
 	}
+
 }
